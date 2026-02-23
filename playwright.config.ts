4a3a755afd9b17ae3624413a41d77533f19cbe34
Romel Rey Silva <sromelrey@gmail.com>
2026-02-23 19:46:42 +0800

    test(e2e): revert to dev server in CI to allow dynamic API mocking

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: process.env.CI ? 'pnpm preview' : 'pnpm dev --port 3000',
+    command: 'pnpm dev --port 3000',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120_000,
